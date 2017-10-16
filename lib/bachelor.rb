674b5e84ce4936a1d65c214a210a2667484ef339
mmb16 <mmb583@nyu.edu>
2017-10-16 14:15:25 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name = ""
+  data.each do |season1, contestant|
+    if season1 == season
+      contestant.each do |array|
+          if array["status"] == "Winner"
+          name = array["name"]
+        end
+      end
+    end
+  end
+  name.split(" ").shift
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season, contestants|
+    contestants.each do |array|
+        if array["occupation"] == occupation
+          name = array["name"]
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |array|
+        if array["hometown"] == hometown
+          counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |array|
+        if array["hometown"] == hometown
+          return array["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  counter = 0
+  data.each do |season1, contestant|
+    if season1 == season
+      contestant.each do |array|
+          age += array["age"].to_f
+          counter += 1
+      end
+    end
+  end
+  avg = age/counter
+  avg.round
 end
